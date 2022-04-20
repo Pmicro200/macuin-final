c33445f0a7071586d990fd82835aade53c64fbc0
Pmicro200 <98743108+Pmicro200@users.noreply.github.com>
2022-04-20 09:20:08 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 55) {
+    if (receivedNumber == 1) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-    } else if (receivedNumber == 888) {
+    } else if (maqueen.Ultrasonic(PingUnit.Centimeters) == 6 || receivedNumber == 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+        radio.sendNumber(20)
     }
 })
 basic.forever(function () {
     radio.setGroup(88)
     music.playMelody("G F G A - F E D ", 120)
     basic.showString("hola")
-})
-basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) == 6) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
-        radio.sendNumber(20)
-    }
 })
 basic.forever(function () {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
